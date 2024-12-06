0b4ef2b3fa1e5eea04acfa90a1e74756375d84cf
martin-b-07e5 <martin.bergagno@gmail.com>
2024-12-06 16:52:39 -0300

    error jackson.databind referencia circular

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,14 +1,25 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+  @Autowired
+  private SerieRepository repository;
+
+
   @GetMapping("/series")
-  public String mostrarMensaje() {
-    return "este es mi primer msg de Series en mi API web - en http://localhost:8080/series";
+  public List<Serie> getAllSeries() {
+    return repository.findAll();
   }
+
 
 }
