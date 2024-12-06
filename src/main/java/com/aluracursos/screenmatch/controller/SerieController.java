a6c636c6c65ec61bef3868765ae9c91249e941f7
martin-b-07e5 <martin.bergagno@gmail.com>
2024-12-06 18:29:36 -0300

    done DTO 04-Representando_series_de_la_base_de_datos

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,13 @@
 package com.aluracursos.screenmatch.controller;
 
-import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.dto.SerieDTO;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
@@ -17,8 +17,16 @@ public class SerieController {
 
 
   @GetMapping("/series")
-  public List<Serie> getAllSeries() {
-    return repository.findAll();
+  public List<SerieDTO> getAllSeries() {
+    return repository.findAll().stream()
+        .map(serie -> new SerieDTO(
+                serie.getTitulo(), serie.getTotalTemporadas(), serie.getEvaluacion(),
+                serie.getPoster(), serie.getGenero(), serie.getActores(),
+                serie.getSinopsis()
+            )
+        )
+        .collect(Collectors.toList());
+
   }
 
 
